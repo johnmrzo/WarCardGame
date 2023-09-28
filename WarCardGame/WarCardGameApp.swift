218d146d62fdbe03c0ba17fdbbf7f37270f5ffb6
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-09-28 18:26:31 -0400

    Card UI is done

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Bakhtiyorjon Mirzajonov on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
